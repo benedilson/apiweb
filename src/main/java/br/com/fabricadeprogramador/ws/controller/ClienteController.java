621b6f345e79c00ee2b0ea35bbf19bf95ce9fd95
benedilson.lima <benedilson.lima@WST-001023.infosolo.com.br>
2018-01-31 14:15:15 -0200

    Token

--- src/main/java/br/com/fabricadeprogramador/ws/controller/ClienteController.java
+++ src/main/java/br/com/fabricadeprogramador/ws/controller/ClienteController.java
@@ -1,12 +1,13 @@
 package br.com.fabricadeprogramador.ws.controller;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,47 +15,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fabricadeprogramador.ws.model.Cliente;
+import br.com.fabricadeprogramador.ws.service.ClienteService;
 
+@Transactional(propagation=Propagation.REQUIRED)
+//@Transactional(propagation=Propagation.NOT_SUPPORTED)
 @RestController
+@RequestMapping("/administrator")
 public class ClienteController {
 	
-	Map<Integer, Cliente> clientes = new HashMap<>();
-	Integer proximoId = 1;
-	
-	//Negocios
-	private Cliente cadastrar(Cliente cliente) {
-		
-		cliente.setId(proximoId);
-		proximoId++;
-		
-		clientes.put(cliente.getId(), cliente);
-		
-		return cliente;
-	}
-	
-	private Collection<Cliente> buscarTodos() {
-		return clientes.values();
-	}
-	
-	private void excluir(Cliente cliente) {
-		clientes.remove(cliente.getId());
-	}
-	
-	private Cliente buscaPorId(Integer id) {
-		return clientes.get(id);
-	}
-	
-	private Cliente alterar(Cliente cliente) {
-		 clientes.put(cliente.getId(), cliente);
-		 
-		 return cliente;
-	}
+	@Autowired
+	ClienteService clienteService;
 	
 	//End points
 	@RequestMapping(method=RequestMethod.POST, value="/clientes", 
 			consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
-		Cliente clienteCadastrado = cadastrar(cliente);
+		Cliente clienteCadastrado = clienteService.cadastrar(cliente);
 		
 		return new ResponseEntity<>(clienteCadastrado, HttpStatus.CREATED);
 		
@@ -63,21 +39,29 @@ public class ClienteController {
 	
 	@RequestMapping(method=RequestMethod.GET, value="/clientes", produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Collection<Cliente>> buscarTodosClientes() {
-		Collection<Cliente> clientesBuscados = buscarTodos();
+		Collection<Cliente> clientesBuscados = clienteService.buscarTodos();
 		
 		return new ResponseEntity<>(clientesBuscados, HttpStatus.OK);
 		
 	}
 	
+	@RequestMapping(method=RequestMethod.GET, value="/clientes/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Cliente> buscarClientesPorId(@PathVariable Integer id) {
+		Cliente cliente = clienteService.buscaPorId(id);
+		
+		return new ResponseEntity<>(cliente, HttpStatus.OK);
+		
+	}
+	
 	@RequestMapping(method=RequestMethod.DELETE, value="/clientes/{id}" )
 	public ResponseEntity<Cliente> excluirCliente(@PathVariable Integer id) {
-		Cliente clienteEncontrado = buscaPorId(id);
+		Cliente clienteEncontrado = clienteService.buscaPorId(id);
 		
 		if(clienteEncontrado == null) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		
-		excluir(clienteEncontrado);
+		clienteService.excluir(clienteEncontrado);
 		
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
@@ -85,7 +69,7 @@ public class ClienteController {
 	@RequestMapping(method=RequestMethod.PUT, value="/clientes", 
 			consumes=MediaType.APPLICATION_JSON_VALUE, produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Cliente> alterarCliente(@RequestBody Cliente cliente) {
-		Cliente clienteAlterado = alterar(cliente);
+		Cliente clienteAlterado = clienteService.alterar(cliente);
 		
 		return new ResponseEntity<>(clienteAlterado, HttpStatus.OK);
 	}
--- src/main/java/br/com/fabricadeprogramador/ws/model/Cliente.java
+++ src/main/java/br/com/fabricadeprogramador/ws/model/Cliente.java
@@ -1,6 +1,17 @@
 package br.com.fabricadeprogramador.ws.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
 public class Cliente {
+	
+	@Id
+	@SequenceGenerator(name="CLIENTE_SEQ", sequenceName="CLIENTE_SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="CLIENTE_SEQ")
 	private Integer id;
 	private String nome;
 	
